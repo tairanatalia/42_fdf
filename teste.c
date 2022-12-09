6e4c66fd0639970f1e082310e5830e5fe20a3bf6
Natalia Gomes taira <ngomes-t@c3r8p5.42sp.org.br>
2022-12-08 21:45:55 -0300

    Trying to put the map inside of the matrix :(

--- teste.c
+++ teste.c
@@ -3,7 +3,10 @@
 #include <fcntl.h>
 #include <errno.h>
 #include "./42_libft/libft.h"
+#include "./includes/fdf.h"
+#include <stdio.h>
 
+/* tratamento de erro */
 void	error_message(char *message)
 {
 	if (errno == 0)
@@ -13,54 +16,97 @@ void	error_message(char *message)
 	exit (1);
 }
 
-int    count_lines(void)
+/* free em char **str */
+void    double_free(char **str)
+{
+    if (*str != NULL)
+        free(*str++);
+}
+
+/* inserir o mapa na matriz */
+void    get_coords(t_dot **dot_matrix, t_mapdim dimensions)
+{
+    char    *line_content;
+    char    **splitted;
+    int     rows;
+    int     cols;
+    int     fd;
+
+    rows = 0;
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        error_message("erro");
+    while (rows < dimensions.rows)
+    {
+        cols = 0;
+        line_content = ft_get_next_line(fd);
+        dot_matrix[rows] = ft_calloc(t_dot, dimensions.cols);
+        while (cols < dimensions.cols)
+        {
+            dot_matrix.x = cols;
+            dot_matrix.y = row;
+            if (ft_strchr(line_content), ',')
+            {
+                splitted = ft_split(line_content, ' ');
+                dot_matrix.z = splitted[0];
+                dot_matrix.color = ft_htoi(splitted[1]);
+                cols++
+            }
+            else
+            {
+                dot_matrix.z = ft_atoi(ft_split[cols]);
+                dot_matriz.color = WHITE;
+                cols++            
+            }
+        }
+        rows++;
+    }
+    free(line_content);
+    double_free(splitted);
+}
+
+
+
+/* medir as dimensões do mapa */
+int    count_lines(char *filename)
 {
     int    fd;
     int    lines;
 
-    fd = open("teste1", O_RDONLY);
+    fd = open(filename, O_RDONLY);
     if (fd < 0)
         error_message("erro");
     lines = 0;
     while (ft_get_next_line(fd) != NULL)
         lines++;
-    close(fd);
     return (lines);
 }
-
-
-int count_columns(void)
+int count_columns(filename)
 {
     int     fd;
     int     columns;
     char    *line;
     char    **splitted;
 
-    fd = open("teste1", O_RDONLY);
+    fd = open(filename, O_RDONLY);
     if (fd < 0)
         error_message("erro");
     line = ft_get_next_line(fd);
-    printf("line = %s", line);
     splitted = ft_split(line, ' ');
     columns = 0;
-    while (*splitted != NULL)
-    {
-        printf("*splitted = %s\n", *splitted);
-        splitted++;
+    while (splitted[0][columns] != NULL)
         columns++;
-    }
+    free(line);
+    double_free(splitted);
     close(fd);
     return (columns);
 }
 
 int main(void)
 {
-    int lines;
-    int columns;
-    char    **splitted;
-    
-    lines = count_lines();
-    columns = count_columns();
-    printf("lines = %d, columns = %d\n", lines, columns);
+    char    *number = "#e35b71";
+    char    *toupper = str_upper(number);
+    printf("%s", toupper);
     return (0);
 }
+
