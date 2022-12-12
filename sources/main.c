bd52fc7ce132135288f2aa0a3f9367633252878b
natalia taira <nataliagtaira@gmail.com>
2022-12-12 02:03:48 +0100

    trying to put the map inside of the struct matrix

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngomes-t <ngomes-t@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ngomes-t <ngomes-t@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 16:12:32 by ngomes-t          #+#    #+#             */
-/*   Updated: 2022/12/08 21:32:06 by ngomes-t         ###   ########.fr       */
+/*   Updated: 2022/12/12 01:58:43 by ngomes-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,63 +14,77 @@
 #include <stdio.h>
 
 /* colocar imagem na janela */
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = img->addr + (y * img->line_length + x * (img->bpp / 8));
 	*(unsigned int*)dst = color;
 }
 
 /* fechar janela */
 int key_hook(int keycode, t_win *win)
 {
-    if (keycode == 65307)
-    {
-        mlx_destroy_window(win->mlx, win->mlx_win);
-        mlx_destroy_display(win->mlx);
-        if (win->mlx)
-            free(win->mlx);
-        exit(1);
-    }
-    printf("%d \n", keycode);
-    return (0);
+	if (keycode == 65307)
+	{
+		mlx_destroy_window(win->mlx, win->mlx_win);
+		mlx_destroy_display(win->mlx);
+		if (win->mlx)
+			free(win->mlx);
+		exit(1);
+	}
+	printf("%d \n", keycode);
+	return (0);
+}
+
+/* free em char **str */
+void	double_free(char **str)
+{
+	if (*str != NULL)
+		free(*str++);
+
 }
 
 /* inserir os mapa na matriz */
-void    get_coords(t_dot dot_matrix[][], t_mapdim dimensions)
+void	get_coords(t_dot **dot_matrix, t_mapdim dimensions, char *filename)
 {
-    char    *line_content;
-    char    **splitted;
+	char	*line_content;
+	char	**splitted;
+	int		rows;
+	int		cols;
+	int		fd;
 
-    rows = 0;
-    while (rows < dimensions.rows)
-    {
-        cols = 0
-        line_content = ft_get_next_line(fd);
-        dot_matrix[rows] = ft_calloc(t_dot, dimensions.cols);
-        while (cols < dimensions.cols)
-        {
-            dot_matrix.x = cols;
-            dot_matrix.y = row;
-            if (ft_strchr(line_content), ',')
-            {
-                splitted = ft_split(line_content, ' ');
-                dot_matrix.z = splitted[0];
-                dot_matrix.color = ft_htoi(splitted[1]);
-                cols++
-            }
-            else
-            {
-                dot_matrix.z = ft_atoi(ft_split[cols]);
-                dot_matriz.color = WHITE;
-                cols++            
-            }
-        }
-        rows++;
-    }
-    free(line_content);
-    double_free(splitted);
+	rows = 0;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		error_message("erro");
+	while (rows < dimensions.rows)
+	{
+		cols = 0;
+		line_content = ft_get_next_line(fd);
+		dot_matrix[rows] = ft_calloc(sizeof(t_dot), dimensions.cols);
+		while (cols < dimensions.cols)
+		{
+			dot_matrix[cols][rows].x = cols;
+			dot_matrix[cols][rows].y = rows;
+			if (ft_strchr(line_content, ','))
+			{
+				splitted = ft_split(line_content, ' ');
+				dot_matrix[cols][rows].z = splitted[0][cols];
+				dot_matrix[cols][rows].color = ft_htoi(splitted[1]);
+				cols++;
+			}
+			else
+			{
+				dot_matrix[cols][rows].z = ft_atoi(ft_split(line_content, ' ')[cols]);
+				dot_matrix[cols][rows].color = WHITE;
+				cols++;
+			}
+		}
+		rows++;
+	}
+	free(line_content);
+	double_free(splitted);
 }
 
 /* tratamento de erro */
@@ -83,93 +97,89 @@ void	error_message(char *message)
 	exit (1);
 }
 
-/* free em char **str */
-void    double_free(char **str)
-{
-    if (*str != NULL)
-        free(*str++);
-}
+
 
 
 /* medir as dimensões do mapa */
-int    count_lines(char *filename)
+int	count_lines(char *filename)
 {
-    int    fd;
-    int    lines;
+	int	fd;
+	int	lines;
 
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        error_message("erro");
-    lines = 0;
-    while (ft_get_next_line(fd) != NULL)
-        lines++;
-    return (lines);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		error_message("erro");
+	lines = 0;
+	while (ft_get_next_line(fd) != NULL)
+		lines++;
+	return (lines);
 }
-int count_columns(filename)
+int	count_columns(char *filename)
 {
-    int     fd;
-    int     columns;
-    char    *line;
-    char    **splitted;
+	int	fd;
+	int	columns;
+	char	*line;
+	char	**splitted;
 
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        error_message("erro");
-    line = ft_get_next_line(fd);
-    splitted = ft_split(line, ' ');
-    columns = 0;
-    while (splitted[0][columns] != NULL)
-        columns++;
-    free(line);
-    double_free(splitted);
-    close(fd);
-    return (columns);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		error_message("erro");
+	line = ft_get_next_line(fd);
+	splitted = ft_split(line, ' ');
+	columns = 0;
+	while (*splitted != NULL)
+		columns++;
+	free(line);
+	double_free(splitted);
+	close(fd);
+	return (columns);
 }
 
 
 
-int main()
+int main(int argc, char **argv)
 {
-    t_win   win;
-    t_img  img;
-    t_dot   **dot_matrix;
-    t_mapdim dimensions;
+	t_win		win;
+	t_img		img;
+	t_dot		**dot_matrix;
+	t_mapdim	dimensions;
+	int			i;
+	int			j;
 
-    int i;
-    int j;
-
-    i = 0;
-    win.mlx = mlx_init();
-    win.mlx_win = mlx_new_window(win.mlx, WIDTH, HEIGHT, "janela");
-    img.img = mlx_new_image(win.mlx, WIDTH, HEIGHT);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+	i = 0;
+	if (argc != 2)
+		error_message("erro");
+	win.mlx = mlx_init();
+	win.mlx_win = mlx_new_window(win.mlx, WIDTH, HEIGHT, "janela");
+	img.img = mlx_new_image(win.mlx, WIDTH, HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_length,
 								&img.endian);
-    while (i < 50)
-    {
-        j = 0;
-        while (j < 50)
-        {
-            my_mlx_pixel_put(&img, i, j, 0XF5A442);
-            j++;
-        }
-        i++;
-    }
-    while (i < 100)
-    {
-        j = 50;
-        while (j < 100)
-        {
-            my_mlx_pixel_put(&img, i, j, 0XF5A442);
-            j++;
-        }
-        i++;
-    }
-    dimensions.cols = count_columns(argv[1]);
-    dimensions.rows = count_lines(argv[1]);
-    dot_matrix = ft_calloc(t_dot *, dimensions.rows);
-    
-    mlx_put_image_to_window(win.mlx, win.mlx_win, img.img, 0, 0);
-    mlx_destroy_image(win.mlx, img.img);
-    mlx_key_hook(win.mlx_win, key_hook, &win);
-    mlx_loop(win.mlx);
+	while (i < 50)
+	{
+		j = 0;
+		while (j < 50)
+		{
+			my_mlx_pixel_put(&img, i, j, 0XF5A442);
+			j++;
+		}
+		i++;
+	}
+	while (i < 100)
+	{
+		j = 50;
+		while (j < 100)
+		{
+			my_mlx_pixel_put(&img, i, j, 0XF5A447);
+			j++;
+		}
+		i++;
+	}
+	dimensions.cols = count_columns(argv[1]);
+	dimensions.rows = count_lines(argv[1]);
+	dot_matrix = ft_calloc(sizeof(t_dot *), dimensions.rows);
+	get_coords(dot_matrix, dimensions, argv[1]);
+	mlx_put_image_to_window(win.mlx, win.mlx_win, img.img, 0, 0);
+	mlx_destroy_image(win.mlx, img.img);
+	mlx_key_hook(win.mlx_win, key_hook, &win);
+	mlx_loop(win.mlx);
 }
